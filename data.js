f2e0e93b83add4ac82239807fde677de78d15025
Anders Forsman <andeye@cyancore.net>
2021-02-14 17:38:12 +0200

    Add data.js with one character as example
    TODO: Check which quotation style to use.

---
+++ data.js
@@ -0,0 +1,46 @@
+var data = {
+	"characters": [
+		{
+			name: "Hulk Hogan",
+			bio: "Hulk Hogan, born 1953, is an american wrestler and celebrity.",
+			filename: "character_01.png",
+			croppedfilename: "cropped_01.png",
+			questions: [
+				{
+					question: "Besides wrestling, Hulk Hogan has an extensive acting career. Which of the following movies has Hulk been in?",
+					answertext: "Hulk Hogan played the antagonistic \"Thunderlips\" in Rocky III. This was his first Hollywood movie role.",
+					answers: [
+						"Rocky III",
+						"The Expendables",
+						"Star Wars"
+					]
+				}, {
+					question: "Which of the following awards has Hulk Hogan not received?",
+					answertext: "Hulk Hogan has won multiple awards, some however less coveted than others.",
+					answers: [
+						"Nobel peace prize",
+						"Best babyface",
+						"Most obnoxious wrestler"
+					]
+				}, {
+					question: "True or False? Hulk Hogan's employer, WWE was previously known as WWF.",
+					answertext: "WWE was forced to switch to it's current name after a lawsuit by the World Wildlife Fund",
+					answers: [
+						"TRUE",
+						"FALSE"
+					]
+				},
+			],
+			funfacts: [
+				'According to IGN, Hulk Hogan is "the most recognized wrestling star worldwide and the most popular wrestler of the 1980s"',
+				"Hulk Hogan's real name is Terry Eugene Bollea.",
+				"According to the Wrestling figure checklist, there have been 171 registered Hulk Hogan action figures.'"
+			],
+			tags: [
+				"1980s",
+				"modern",
+				"wrestling"
+			]
+		}
+	]
+}
