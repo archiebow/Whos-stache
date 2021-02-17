79af229ba98a38ec96530f9ac65454107149cc0c
Arthur Tolllet <arthur.tollet@aalto.fi>
2021-02-17 14:05:36 +0200

    xlsx converter function implemented

--- converter.py
+++ converter.py
@@ -1,28 +1,50 @@
 from openpyxl import load_workbook
 import json
 
+def importcharacter(ws): #reads an xlsx worksheet and returns a dict object with the character data.
+    character = {
+    'name': '',
+    'bio': '',
+    'questions' : [],
+    'funfacts' : [],
+    'tags' : [],
+
+    }
+
+    for row in ws.values:
+        if row[0] == 'Name':
+            character['name'] = row[1]
+        elif row[0] == 'Bio':
+            character['bio'] = row[1]
+        elif row[0] == 'Q':
+            i = 3
+            ans = []
+            while i < len(row) and row[i] != None:
+                ans.append(row[i])
+                i = i+1
+
+            character['questions'].append({
+                'question' : row[1],
+                'answertext' : row[2],
+                'answers' : ans      
+            }
+            )
+        elif row[0] == 'FF':
+            character['funfacts'].append(row[1])
+        elif row[0] == 'Tags':
+            i = 1
+            tag = []
+            while i < len(row) and row[i] != None:
+                tag.append(row[i])
+                i = i+1
+            character['tags'] = tag
+        else:
+            break
+    return character
+
+
 wb = load_workbook(filename = 'data.xlsx')
-sheet = wb['Taulukko1']
+ws = wb['Taulukko1']
 
-character = {
-    'name':
-    'bio':
-    'questions' : []
-    'funfacts' : []
-    'tags' : []
-
-}
-
-row = 1
-while True:
-    if sheet['A'+str(row)].value == None:
-        break
-    switch(sheet['A'+str(row)].value.lower)
-    case 'name': character.name = sheet['B'+str(row)]
-    case 'bio': character.bio = sheet['B'+str(row)]
-    case 'Q': questions.append({
-        question : sheet['B'+str(row)]
-        anwertext : sheet['C'+str(row)]
-    })
-    default break
-    row = row+1
+charjson = json.dumps(importcharacter(ws), indent=4)
+print(charjson)
