89d3f3cef0e4f97d84ebece790b08284e9b18075
Anders Forsman <andeye@cyancore.net>
2021-02-18 21:59:58 +0200

    Implement part of core game play loop

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
+function randomRange(excludedMax) {
+	return Math.floor(Math.random() * excludedMax);
+}
+
+function getRandomCharacter() {
+	id = randomRange(data.characters.length);
+	character = data.characters[id];
+	character.id = id;
+	return character;
+}
+
 $(document).ready(function(e) {
-	console.log(data.characters[0].questions[0].answertext);
-	console.log(data.characters[0].funfacts[0]);
+	characterQuestion();
 });
+
+function characterQuestion() {
+	hasAnswered = false;
+	character = getRandomCharacter();
+	console.log(character);
+	console.log(character.name, character.id);
+	$("#image").css("background-image", "url('stash.jpg')");
+	var question = randomRange(character.questions.length);
+	$("#question").text(character.questions[question].question);
+	var answers = character.questions[question].answers;
+	answerText = character.questions[question].answertext;
+	var answerList = [];
+	for (idx in answers) {
+		var element = $(`<div class="answer alternative">${answers[idx]}</div>`);
+		if (idx == 0) {
+			rightAnswer = element;
+		}
+		(function(index) {
+			element.click(function(event) {
+				answerClick(event, index);
+			});
+			answerList.push(element);
+		})(idx);
+	}
+	while (answerList.length > 0) {
+		var index = randomRange(answerList.length);
+		var element = answerList.splice(index, 1)[0]
+		element.appendTo("#answers");
+	}
+}
+
+function answerClick(event, answer) {
+	if (!hasAnswered) {
+		hasAnswered = true;
+		if (answer == 0) {
+			$(event.target).addClass("right");
+		} else {
+			$(event.target).addClass("wrong");
+			rightAnswer.addClass("right");
+		}
+		var answerElements = $(".answer");
+		$("#answertext").text(answerText);
+		$("#answertext").css("animation", "fade-in 1s");
+		$("#answers").empty();
+		$("#answertext").text("");
+		characterQuestion();
+	}
+}
