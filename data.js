580802cbc1d001dc00ce05391a4a2c2c22d39d7c
archiebow <79049689+archiebow@users.noreply.github.com>
2021-02-23 13:03:37 +0200

    New datafile from converter, as well as related image directory

--- data.js
+++ data.js
@@ -1,46 +1,338 @@
 var data = {
 	"characters": [
-		{
-			name: "Hulk Hogan",
-			bio: "Hulk Hogan, born 1953, is an american wrestler and celebrity.",
-			filename: "character_01.png",
-			croppedfilename: "cropped_01.png",
-			questions: [
-				{
-					question: "Besides wrestling, Hulk Hogan has an extensive acting career. Which of the following movies has Hulk been in?",
-					answertext: "Hulk Hogan played the antagonistic \"Thunderlips\" in Rocky III. This was his first Hollywood movie role.",
-					answers: [
-						"Rocky III",
-						"The Expendables",
-						"Star Wars"
-					]
-				}, {
-					question: "Which of the following awards has Hulk Hogan not received?",
-					answertext: "Hulk Hogan has won multiple awards, some however less coveted than others.",
-					answers: [
-						"Nobel peace prize",
-						"Best babyface",
-						"Most obnoxious wrestler"
-					]
-				}, {
-					question: "True or False? Hulk Hogan's employer, WWE was previously known as WWF.",
-					answertext: "WWE was forced to switch to it's current name after a lawsuit by the World Wildlife Fund",
-					answers: [
-						"TRUE",
-						"FALSE"
-					]
-				},
-			],
-			funfacts: [
-				'According to IGN, Hulk Hogan is "the most recognized wrestling star worldwide and the most popular wrestler of the 1980s"',
-				"Hulk Hogan's real name is Terry Eugene Bollea.",
-				"According to the Wrestling figure checklist, there have been 171 registered Hulk Hogan action figures.'"
-			],
-			tags: [
-				"1980s",
-				"modern",
-				"wrestling"
-			]
-		}
-	]
+     {
+          "name": "Adolf Hiler",
+          "bio": "German leader during the second world war. Started the war\nby invading Poland in 1939 and commited war crimes\nduring the war such as the holocaust. Commited his own life in\n1945. \n",
+          "filename": "photodir\\0.jpg",
+          "croppedfilename": "photodir\\0.png",
+          "questions": [
+               {
+                    "question": "When did the famous Beer Hall Push witch Hitler lead happen?",
+                    "answertext": "1923 in a Beer Hall in Munich witch lead to\na prison sentence to Hitler",
+                    "answers": [
+                         1923.0,
+                         1921.0,
+                         1930.0
+                    ]
+               },
+               {
+                    "question": "What does hitler byname \"the Fuhrer\" mean?",
+                    "answertext": "Hitler became closley linked with the name \nDie Fuhrer during the war, he first used it in\n1934 when he became the sole ruler of Germany",
+                    "answers": [
+                         "The leader",
+                         "The father",
+                         "The lruler"
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Tried to become an artist but failed the entrence exam"
+          ],
+          "tags": [
+               "WW2",
+               "German",
+               "Nazis",
+               "..."
+          ]
+     },
+     {
+          "name": "Albert Edelfelt",
+          "bio": "Albert Edelfelt, 1854-1905, is one of Finlands most known painters. (More to come)",
+          "filename": "photodir\\1.jpg",
+          "croppedfilename": "photodir\\1.png",
+          "questions": [
+               {
+                    "question": "What was Albert Edelfelts whole name?",
+                    "answertext": "Georg Theodor Policron Chiewitz was a finnish architect and Wanold Wrydon Wilkman a finnish geologist.",
+                    "answers": [
+                         "Albert Gustaf Aristides Edelfelt",
+                         "Albert Theodor Policron Edelfelt",
+                         "Albert Wanold Wrydon Edelfelt"
+                    ]
+               },
+               {
+                    "question": "Albert Edelfelt died only 51 years old. What was the reason?",
+                    "answertext": "Edelfelts friend, Ville Vallgren, tought that one reason for the heart attack was that he had relasionships with many different women.",
+                    "answers": [
+                         "Heart attack",
+                         "Typhus",
+                         "He was hit by a tram"
+                    ]
+               },
+               {
+                    "question": "It is believed that Edelefelts painting, Dopf\u00e4rd (Risti\u00e4issaatto), that was sold on auction in Finland in 2017, is the most expensive Edelfet painting ever. What was it sold for?",
+                    "answertext": "The painting was bought by Anders Wikl\u00f6f, an art collector from the \u00c5land Islands.",
+                    "answers": [
+                         637000.0,
+                         512000.0,
+                         898000.0
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Edelfelts last grand work was the Inauguration of the Academy of \u00c5bo, finnished in 1905. It was destroyd during the Soviet bombardement of Helsinki in 1944."
+          ],
+          "tags": [
+               "1800s",
+               "painter",
+               "finnish"
+          ]
+     },
+     {
+          "name": "Hulk Hogan",
+          "bio": "Hulk Hogan, born 1953, is an american wrestler and celebrity.",
+          "filename": "photodir\\2.jpg",
+          "croppedfilename": "photodir\\2.png",
+          "questions": [
+               {
+                    "question": "Besides wrestling, Hulk Hogan has an extensive acting career. Which of the following movies has Hulk been in?",
+                    "answertext": "Hulk Hogan played the antagonistic \"Thunderlips\" in Rocky III. This was his first Hollywood movie role.",
+                    "answers": [
+                         "Rocky III",
+                         "The Expendables",
+                         "Star Wars"
+                    ]
+               },
+               {
+                    "question": "Which of the following awards has Hulk Hogan not received?",
+                    "answertext": "Hulk Hogan has won multiple awards, some however less coveted than others.",
+                    "answers": [
+                         "Nobel peace prize",
+                         "Best babyface",
+                         "Most obnoxious wrestler"
+                    ]
+               },
+               {
+                    "question": "True or False? Hulk Hogan's employer, WWE was previously known as WWF.",
+                    "answertext": "WWE was forced to switch to it's current name after a lawsuit by the World Wildlife Fund",
+                    "answers": [
+                         true,
+                         false
+                    ]
+               }
+          ],
+          "funfacts": [
+               "According to IGN, Hulk Hogan is \"the most recognized wrestling star worldwide and the most popular wrestler of the 1980s\"",
+               "Hulk Hogan's real name is Terry Eugene Bollea.",
+               "According to the Wrestling figure checklist, there have been 171 registered Hulk Hogan action figures."
+          ],
+          "tags": [
+               "1980s",
+               "modern",
+               "wrestling"
+          ]
+     },
+     {
+          "name": "Gustaf II Adolf",
+          "bio": "King over Sweden in the mid 17th century\nand lead the swedish army in the 30 years war \nestablishing Sweden as a great power in Europe",
+          "filename": "photodir\\3.jpg",
+          "croppedfilename": "photodir\\3.png",
+          "questions": [
+               {
+                    "question": "At which famous battle did Gustaf II Adolf die aged \n37 ",
+                    "answertext": "He died at the battle while riding a \na horse straight in to the enemy in thick\nfog",
+                    "answers": [
+                         "Battle of Lutzen",
+                         "Battle of Breitenfeld",
+                         "Battle of N\u00f6rdlingen"
+                    ]
+               }
+          ],
+          "funfacts": [
+               "He is known as the Lion of the north and has \nbecome known under that name in songs and\nother pop culture/history"
+          ],
+          "tags": [
+               "Sweden",
+               "King",
+               "Religion",
+               "17'th century"
+          ]
+     },
+     {
+          "name": "Heinrich Himmler",
+          "bio": "Himmler was the leader of the Waffen SS\nand the maine architect behind the holocaust",
+          "filename": "photodir\\4.jpg",
+          "croppedfilename": "photodir\\4.png",
+          "questions": [
+               {
+                    "question": "When did Himmler join the NSDAP?(nazi party)",
+                    "answertext": "Himmler joined at the very start in 1923 and\nbecame a leading figure in the party  ",
+                    "answers": [
+                         1923.0,
+                         1933.0,
+                         1930.0
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Himmler studied agronomy before joining the NSDAP"
+          ],
+          "tags": [
+               "WW2",
+               "German",
+               "Nazis",
+               "..."
+          ]
+     },
+     {
+          "name": "Henrik Meinander",
+          "bio": "Henrik Meinander, born 1960, is the professor in history in Swedish at the University of Helsinki since 2002.",
+          "filename": "photodir\\5.jpg",
+          "croppedfilename": "photodir\\5.png",
+          "questions": [
+               {
+                    "question": "Henrik Meinanders father was known as well, but for what?",
+                    "answertext": "Carl Fredrik Meinander was a Finnish archaeologist and professor of Finnish and Scandinavian archaeology at the University of Helsinki.",
+                    "answers": [
+                         "He was a professor at Helsinki University",
+                         "He was one of the first Finnish professional football players",
+                         "He was a renowned alcohol smuggler that never got prosecuted or convicted"
+                    ]
+               },
+               {
+                    "question": "Henrik Meinander has acted as the Inspector for a Finnish Nation, which one?",
+                    "answertext": "Henrik was the Inspector for Nylands nation between the years 2010-2018. His father had the same position between the years 1972-1978.",
+                    "answers": [
+                         "Nylands nation",
+                         "Varsinaissuomalainen osakunta",
+                         "\u00c5bo nation"
+                    ]
+               },
+               {
+                    "question": "In 2009 Meinander published a book about a year in Finnish history. What year?",
+                    "answertext": "The book, published in Swedish and translated to Finnish, was called Finland 1944: Krig, samh\u00e4lle, k\u00e4nslolandskap/Suomi 1944: Sota, yhteiskunta, tunnemaisema.",
+                    "answers": [
+                         1944,
+                         1939,
+                         1918
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Meinander worked as the curator for the Mannerheim museum in Helsinki and published a book about Mannerheim in 2017 named Gustaf Mannerheim: Aristokrat i vadmal/Gustaf Mannerheim: Aristokraatti sarkatakissa."
+          ],
+          "tags": [
+               "2000s",
+               "professor",
+               "finnish",
+               "author"
+          ]
+     },
+     {
+          "name": "Leo Mechelin",
+          "bio": "Leo Mechelin, 1839-1914, was as Finnish liberal politician. (More info to come..)",
+          "filename": "photodir\\6.jpg",
+          "croppedfilename": "photodir\\6.png",
+          "questions": [
+               {
+                    "question": "What year did Leo Mechelin form the so called Mechelin-Senate with K.J. S\u00e5hlberg, Kasten Antell and Otto Donner among others?",
+                    "answertext": "The Mechelin-Senate was in power between December 1905 to March 1908.",
+                    "answers": [
+                         1905,
+                         1901,
+                         1909
+                    ]
+               },
+               {
+                    "question": "In 1877 Mechelin was knighted. To what level?",
+                    "answertext": "Tough knighted, he was exiled about 25 years later, between the years 1903-1904.",
+                    "answers": [
+                         "Nobleman",
+                         "Baron",
+                         "Count"
+                    ]
+               },
+               {
+                    "question": "Which company did Mechelin found together with his friend Fredrik Idestam?",
+                    "answertext": "The old Nokia company (a pulp company) was merged with Finska Gummi (rubber products) and Finska Kabelfabriken (cabel produkts) in 1967 to form the new Nokia.",
+                    "answers": [
+                         "Nokia",
+                         "W\u00e4rtsil\u00e4",
+                         "Fiskars"
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Leo Mechelin married Alexandra Lindroos, who was the daughter of the richest merchant in Helsingfors at the time. They had only one daughter, Cely, who died childless. She left all of her fortune to Svenska litteraturs\u00e4llskapet."
+          ],
+          "tags": [
+               "1800s",
+               "politician",
+               "liberal",
+               "finnish"
+          ]
+     },
+     {
+          "name": "Napoleon III",
+          "bio": "Napoleon III (1808-1873) was the first president of France, and the last French Monarch",
+          "filename": "photodir\\7.jpg",
+          "croppedfilename": "photodir\\7.png",
+          "questions": [
+               {
+                    "question": "How was Napoleon III related to Napoleon Bonaparte?",
+                    "answertext": "Napoleon III was child to Napoleon I's brother, and to his wife's daughter from a previous marriage.",
+                    "answers": [
+                         "Nephew",
+                         "Son",
+                         "Brother"
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Napoleon III was captured by the prussian army during the Battle of Sedan in 1870, leading to French surrender.",
+               "Napoleon III only took power in France after his third attempt at a coup. In fact the usual punishment of death was commuted to prison time and exile, since he was viewed as not being much of a threat."
+          ],
+          "tags": [
+               "Historical",
+               "1800s",
+               "French",
+               "Kings"
+          ]
+     },
+     {
+          "name": "Zacharias Topelius",
+          "bio": "Zacharias Topelius, 1818-1898, was a finnish editor, teacher and author. (More to come)",
+          "filename": "photodir\\8.jpg",
+          "croppedfilename": "photodir\\8.png",
+          "questions": [
+               {
+                    "question": "In 1833 Topelius began studying at the University. What did he plan to study?",
+                    "answertext": "Topelius was more interested in art subjects and received his PhD in history in 1847. He became extraordinary professor in Finnsh history in 1854 and ordinary professor of Finnish, Nordic and Russian history in 1863.",
+                    "answers": [
+                         "Medicin",
+                         "Archeology",
+                         "Biology"
+                    ]
+               },
+               {
+                    "question": "What year was Boken om v\u00e5rt land/Maamme-kirja/Book of Our Land published?",
+                    "answertext": "L\u00e4sning f\u00f6r barn/Lukemista lapsille was published in 1881 and F\u00e4ltsk\u00e4rn ber\u00e4ttelser/V\u00e4lsk\u00e4rin kertomukset in 1884.",
+                    "answers": [
+                         1875.0,
+                         1884.0,
+                         1881.0
+                    ]
+               },
+               {
+                    "question": "One of the most beloved Christmas songs in Finland is based on a poem by Topelius. Which girl name is found in the title?",
+                    "answertext": "It was voted as the most beautiful Finnish Christmas song by Yle in the 1960's and again in 2002.",
+                    "answers": [
+                         "Sylvia",
+                         "Sonja",
+                         "Saga"
+                    ]
+               }
+          ],
+          "funfacts": [
+               "Topelius was Rector for the University between 1875-1878. After that he received the rare title verkligt statsr\u00e5d/todellinen valtioneuvos"
+          ],
+          "tags": [
+               "1800s",
+               "author",
+               "teacher",
+               "editor",
+               "finnish"
+          ]
+     }
+]
 }
